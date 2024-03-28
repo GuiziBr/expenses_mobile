9ef9e94bf3fd3ab365d5a331ecfada07bbe82bec
Ricardo Guizi <guizi189@gmail.com>
2024-03-27 20:57:17 -0700

    update api to handle errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,32 @@
 import { AppError } from '@utils/AppError'
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
-const api = axios.create({ baseURL: 'http://192.168.1.77:3333' })
+type SignOut = () => void
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void
+}
 
-api.interceptors.response.use(response => response, (error) => {
-  if(error.response && error.response.data) {
-    return Promise.reject(new AppError(error.response.data.message))
+const api = axios.create({ baseURL: 'http://192.168.1.77:3333' }) as APIInstanceProps
+
+api.registerInterceptTokenManager = signOut => {
+  const interceptTokenManager = api.interceptors.response.use(response => response, requestError => {
+
+    if(requestError?.response?.status === 401) {
+      if(requestError.response.data?.message === 'Invalid JWT token') {
+        signOut()
+        return Promise.reject(new AppError('Session expired, please sign in again'))
+      }
+    }
+
+    if(requestError.response && requestError.response.data) {
+      return Promise.reject(new AppError(requestError.response.data.message))
+    }
+    return Promise.reject(new AppError('Server error, try again later'))
+  })
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager)
   }
-  return Promise.reject(new AppError('Server error, try again later'))
-})
+}
 
 export { api }
